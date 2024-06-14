937da30be6bf6d3d23ab4e61dd0a482c265b1be9
Tao Huang <countcainht@gmail.com>
2024-06-14 10:53:27 -0400

    apply mapping to buffer exploer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,38 +6,38 @@ return {
       { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
     },
     opts = {
+      window = {
+        mappings = {
+          ["[h"] = "prev_git_modified",
+          ["]h"] = "next_git_modified",
+          ["h"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" and node:is_expanded() then
+              state.commands["toggle_node"](state)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+            end
+          end,
+          ["l"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" then
+              if not node:is_expanded() then
+                state.commands["toggle_node"](state)
+              elseif node:has_children() then
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+              end
+            end
+            if node.type == "file" then
+              state.commands["open"](state)
+            end
+          end,
+        },
+      },
       filesystem = {
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
-          hide_gitignored = true,
-        },
-        window = {
-          mappings = {
-            ["[h"] = "prev_git_modified",
-            ["]h"] = "next_git_modified",
-            ["h"] = function(state)
-              local node = state.tree:get_node()
-              if node.type == "directory" and node:is_expanded() then
-                state.commands["toggle_node"](state)
-              else
-                require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-              end
-            end,
-            ["l"] = function(state)
-              local node = state.tree:get_node()
-              if node.type == "directory" then
-                if not node:is_expanded() then
-                  state.commands["toggle_node"](state)
-                elseif node:has_children() then
-                  require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
-                end
-              end
-              if node.type == "file" then
-                state.commands["open"](state)
-              end
-            end,
-          },
+          hide_gitignored = false,
         },
       },
     },
