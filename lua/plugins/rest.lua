06e6050bf63f8e09328abb6a6f4c0d66de5b3ed0
Tao Huang <countcainht@gmail.com>
2024-09-03 10:44:46 -0400

    bump versions

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -10,29 +10,13 @@ return {
     dependencies = { "luarocks.nvim" },
     config = function()
       require("rest-nvim").setup({
-        keybinds = {
-          {
-            "<leader>rr",
-            "<cmd>Rest run<cr>",
-            "http rest: run request under the cursor",
-          },
-          {
-            "<leader>rl",
-            "<cmd>Rest run last<cr>",
-            "http rest: re-run last request",
-          },
-          {
-            "<leader>rt",
-            "<cmd>Telescope rest select_env<cr>",
-            "http rest: select env file",
-          },
-        },
-        result = {
-          keybinds = {
-            buffer_local = true,
-          },
+        env = {
+          pattern = "%.env$",
         },
       })
+      vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", { desc = "http rest: run request under the cursor" })
+      vim.keymap.set("n", "<leader>rl", "<cmd>Rest run last<cr>", { desc = "http rest: re-run last request" })
+      vim.keymap.set("n", "<leader>rt", "<cmd>Rest env select<cr>", { desc = "http rest: select env file" })
     end,
   },
 }
